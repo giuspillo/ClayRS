505440f756f4b66fb8d318f2d35784f637cc8128
Giuseppe Spillo <44213842+giuspillo@users.noreply.github.com>
2024-12-10 10:42:03 +0100

    Update __init__.py

--- clayrs/evaluation/metrics/__init__.py
+++ clayrs/evaluation/metrics/__init__.py
@@ -1,11 +1,6 @@
-from .classification_metrics import Precision, PrecisionAtK, RPrecision, Recall, RecallAtK, \
-    FMeasure, FMeasureAtK
+from .classification_metrics import Precision, PrecisionAtK, RPrecision, Recall, RecallAtK, FMeasure, FMeasureAtK
 from .error_metrics import MAE, MSE, RMSE
-<<<<<<< HEAD
 from .fairness_metrics import GiniIndex, DeltaGap, PredictionCoverage, CatalogCoverage, AvgPopularity, AvgPopularityAtK, FairnessMetric, EPC, APLT
-=======
-from .fairness_metrics import GiniIndex, DeltaGap, PredictionCoverage, CatalogCoverage, AvgPopularity, AvgPopularityAtK, FairnessMetric
->>>>>>> e4cd5423b39a3cdbda2f9558949c3e1c304678a0
 from .plot_metrics import PopRatioProfileVsRecs, PopRecsCorrelation, LongTailDistr
 from .ranking_metrics import NDCG, NDCGAtK, MRR, MRRAtK, Correlation, MAP
 from .metrics import Metric
